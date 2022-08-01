16ced89afbbad23c586da1b2089c5f7e185d9fdc
Fernando Oyanarte <fernando.oyanarte@gmail.com>
2022-08-01 13:14:47 -0300

    corrigiendo variables

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let partidosGanados
 let partidosEmpatados 
 let partidosPerdidos 
 let golesFavor 
-let golesContra 
+let golesContra
 function puntos(partidosGanados, partidosEmpatados, partidosPerdidos) {
     return partidosGanados*3 + partidosEmpatados*1 + partidosPerdidos
 }
@@ -18,27 +18,27 @@ do{
         alert("Ingrese un equipo para continuar")
         continue        
     } 
-     partidosGanados = parseFloat(prompt("Ingrese partidos ganados"))
+    partidosGanados = parseFloat(prompt("Ingrese partidos ganados"))
     if (partidosGanados<0) {
         alert("Ingrese un número válido")
         continue      
     }    
-     partidosEmpatados = parseFloat(prompt("Ingrese partidos empatados"))
+    partidosEmpatados = parseFloat(prompt("Ingrese partidos empatados"))
     if (partidosEmpatados<0){
         alert("Ingrese un número válido")
         continue 
     } 
-     partidosPerdidos = parseFloat(prompt("Ingrese partidos perdidos"))
+    partidosPerdidos = parseFloat(prompt("Ingrese partidos perdidos"))
     if (partidosPerdidos<0){
         alert("Ingrese un número válido")
         continue
     } 
-     golesFavor = parseFloat(prompt("Ingrese goles a favor"))
+    golesFavor = parseFloat(prompt("Ingrese goles a favor"))
     if (golesFavor<0){
         alert("Ingrese un número válido")
         continue
     } 
-     golesContra = parseFloat(prompt("Ingrese goles en contra"))
+    golesContra = parseFloat(prompt("Ingrese goles en contra"))
     if (golesContra<0){
         alert("Ingrese un número válido")
         continue    
