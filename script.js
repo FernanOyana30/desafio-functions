9daaacf6d6ba97ee145621c7ceeac991d89c3087
Fernando Oyanarte <fernando.oyanarte@gmail.com>
2022-08-01 13:04:49 -0300

    corrigiendo variables

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var equipo = true
+let equipo
+let partidosGanados 
+let partidosEmpatados 
+let partidosPerdidos 
+let golesFavor 
+let golesContra 
 function puntos(partidosGanados, partidosEmpatados, partidosPerdidos) {
     return partidosGanados*3 + partidosEmpatados*1 + partidosPerdidos
 }
@@ -8,32 +13,32 @@ function difGol(golesFavor=0, golesContra=0) {
 }
 
 do{
-    var equipo = prompt("Ingrese un equipo")    
+    equipo = prompt("Ingrese un equipo")    
     if (equipo == "") {
         alert("Ingrese un equipo para continuar")
         continue        
     } 
-    var partidosGanados = parseFloat(prompt("Ingrese partidos ganados"))
+     partidosGanados = parseFloat(prompt("Ingrese partidos ganados"))
     if (partidosGanados<0) {
         alert("Ingrese un número válido")
         continue      
     }    
-    var partidosEmpatados = parseFloat(prompt("Ingrese partidos empatados"))
+     partidosEmpatados = parseFloat(prompt("Ingrese partidos empatados"))
     if (partidosEmpatados<0){
         alert("Ingrese un número válido")
         continue 
     } 
-    var partidosPerdidos = parseFloat(prompt("Ingrese partidos perdidos"))
+     partidosPerdidos = parseFloat(prompt("Ingrese partidos perdidos"))
     if (partidosPerdidos<0){
         alert("Ingrese un número válido")
         continue
     } 
-    var golesFavor = parseFloat(prompt("Ingrese goles a favor"))
+     golesFavor = parseFloat(prompt("Ingrese goles a favor"))
     if (golesFavor<0){
         alert("Ingrese un número válido")
         continue
     } 
-    var golesContra = parseFloat(prompt("Ingrese goles en contra"))
+     golesContra = parseFloat(prompt("Ingrese goles en contra"))
     if (golesContra<0){
         alert("Ingrese un número válido")
         continue    
